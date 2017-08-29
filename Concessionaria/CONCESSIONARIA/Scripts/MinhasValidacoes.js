9df57b054897e941d3b59113022047571c6b4422
Rafael Henrique <rafael.henriquemorais@gmail.com>
2017-08-29 09:16:24 -0300

    -

--- Concessionaria/CONCESSIONARIA/Scripts/MinhasValidacoes.js
+++ Concessionaria/CONCESSIONARIA/Scripts/MinhasValidacoes.js
@@ -1,32 +1,36 @@
-﻿function Chars() {
-    evt = window.event;
-    var tecla = evt.keyCode;
-
-    if (!(tecla > 64 && tec < 91) ||
-        (tecla > 96 && tecla < 123)) {
-        alert('Precione apenas letras');
-        evt.preventDefault();
-    }
-}
+﻿
 
 function Nums() {
-    evt = window.event;
-    var tecla = evt.keyCode;
+    var evt = window.event;
+    var tec = evt.keyCode;
 
-    if (tecla < 48 || tecla > 57) {
+    if (tec < 48 || tec > 57) {
         alert('Pressione apenas numeros');
         evt.preventDefault();
     }
 }
 
 function NumsNChars() {
-    evt = window.event;
+    var evt = window.event;
     var tec = evt.keyCode;
-    if(!((tec == 44) || (tec == 32)|| (tec > 48 && tec < 57) ||
-        (tec > 64 && tec < 91) || 
-        (tec > 96 && tec < 122)
-        )) {
+    // console.log(tec.keyCode);
+    if (!((tec == 44) || (tec == 32) || (tec > 48 && tec < 57) ||
+        (tec > 64 && tec < 91) || (tec > 96 && tec < 122))) {
         alert('Pressione letras ou numeros');
+        evt.preventDefault();
+    }
+}
+
+function Chars() {
+    var evt = window.event;
+    var tec = evt.keyCode;
+    console.log(tec.keyCode);
+    if (!(
+        (tec == 32) ||
+        ((tec > 64) && (tec < 91)) ||
+        ((tec > 96) && (tec < 123))
+        )) {
+        alert('Pressione apenas letras');
         evt.preventDefault();
     }
 }
